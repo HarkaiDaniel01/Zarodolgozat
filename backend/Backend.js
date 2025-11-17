807b4134c5fb397e5f8487c8e6a49a5bd0f3ea2b
Potatooffical <danorb12@gmail.com>
2025-11-17 11:20:28 +0100

    frontend elkészitése

--- backend/Backend.js
+++ backend/Backend.js
@@ -107,7 +107,22 @@ app.post('/kerdesekNehez', (req, res) => {
         return res.status(200).json(result)
         })
 })
-
+//jatekosnev felvitele
+app.post('/ujJatekos', 
+    body('jatekos_nev').isLength({min:1}).withMessage('A játékos név megadása kötelező!'),
+    (req, res) => {
+        const validationError=handleValidationErrors(req,res)
+        if (validationError) return validationError
+        const {jatekos_nev} =req.body
+        const sql=`INSERT INTO jatekosok (jatekos_nev) VALUES (?)`
+        pool.query(sql,[jatekos_nev], (err, result) => {
+        if (err) {
+            console.log(err)
+            return res.status(500).json({error:"Hiba"})
+        }
+        return res.status(201).json({message:"Sikeres játékot! :) "})
+        })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* itt lesz a menu */}
+      {/*majd ami a diszités makd egy hater lenne amit majd kigeneralok*/}
+    </div>
+  );
+}
+
+export default App;
