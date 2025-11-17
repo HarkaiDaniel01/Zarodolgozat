73d05852444fcf08560a5ea21593575e8300728c
Harkai Dániel <dani20010924@gmail.com>
2025-11-17 13:39:48 +0100

    vegyes kérdések lekérdezések és cors hiba javítás

--- backend/Backend.js
+++ backend/Backend.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const app = express()
 const port = 3000
 
-app.use(cors)
+app.use(cors())
 app.use(express.json())
 app.use("/kepek",express.static("kepek"))
 
@@ -111,7 +111,65 @@ app.post('/kerdesekNehez', (req, res) => {
         })
 })
 
+//könnyű kérdések vegyes kategória
+app.get('/kerdesekKonnyuVegyes', (req, res) => {
+        const sql=`SELECT * 
+                from kerdesek
+                where kerdesek_nehezseg = 1 
+                ORDER BY rand()
+                LIMIT 3`
+        pool.query(sql, (err, result) => {
+        if (err) {
+            console.log(err)
+            return res.status(500).json({error:"Hiba"})
+        }
+        if (result.length===0){
+            return res.status(404).json({error:"Nincs adat"})
+        }
 
+        return res.status(200).json(result)
+        })
+})
+
+//közepes kérdések vegyes kategória
+app.get('/kerdesekKozepesVegyes', (req, res) => {
+        const sql=`SELECT * 
+                from kerdesek
+                where kerdesek_nehezseg = 2 
+                ORDER BY rand()
+                LIMIT 3`
+        pool.query(sql, (err, result) => {
+        if (err) {
+            console.log(err)
+            return res.status(500).json({error:"Hiba"})
+        }
+        if (result.length===0){
+            return res.status(404).json({error:"Nincs adat"})
+        }
+
+        return res.status(200).json(result)
+        })
+})
+
+//közepes kérdések vegyes kategória
+app.get('/kerdesekNehezVegyes', (req, res) => {
+        const sql=`SELECT * 
+                from kerdesek
+                where kerdesek_nehezseg = 3 
+                ORDER BY rand()
+                LIMIT 4`
+        pool.query(sql, (err, result) => {
+        if (err) {
+            console.log(err)
+            return res.status(500).json({error:"Hiba"})
+        }
+        if (result.length===0){
+            return res.status(404).json({error:"Nincs adat"})
+        }
+
+        return res.status(200).json(result)
+        })
+})
 
 //Gergő végpontjai
 //jatekosnev felvitele
